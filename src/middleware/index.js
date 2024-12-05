9609cc635999236b90c0b030ebc21df71a8949a9
John Barlow <jnbarlow@gmail.com>
2024-12-04 23:44:01 -0500

    updated libs, used loglevel-colors

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,13 +2,13 @@ const pathResolver = require('path');
 const _ = require('lodash');
 
 module.exports = (loglevel) => {
-    const log = require('../logger')(loglevel, 'NodeBox Middleware');
+    const log = require('loglevel-colors')('NodeBox Middleware', loglevel);
     return async (req, res, next) => {
         log.debug('Processing Request');
 
         const path = req.path.substring(1);
         let explodedPath = path.split('/');
-        
+
         if(explodedPath.length < 2) {
             log.info('Using default handler: main.home');
             explodedPath = ['main', 'home'];
@@ -22,7 +22,7 @@ module.exports = (loglevel) => {
             const Handler = require(pathResolver.resolve(requirePath));
             const handler = new Handler(req, res);
             let shouldContinue = true;
-            
+
             //fire handler's preEvent
             if(_.isFunction(handler.preEvent)){
                 log.debug('Firing preEvent.');
@@ -39,13 +39,13 @@ module.exports = (loglevel) => {
                     await handler.postEvent();
                 }
             }
-            
-            
+
+
         } catch (e) {
             log.error(`Attempted ${requirePath}, ${fcn}()`);
             log.error(e);
         }
-        
+
         next();
     };
 }
--- testsite/index.js
+++ testsite/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const Nodebox = require('../index').Nodebox;
+const Nodebox = require('nodebox-framework').Nodebox;
 
 const nodeBox = new Nodebox({loglevel:'debug'});
 
